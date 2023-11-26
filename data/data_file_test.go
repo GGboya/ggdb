9556c18616a3550570909fa487517a648d7f77e4
GGboya <630591905@qq.com>
2023-11-26 12:32:35 +0800

    增加数据文件的CRUD

--- data/data_file_test.go
+++ data/data_file_test.go
@@ -14,3 +14,42 @@ func TestOpenDataFile(t *testing.T) {
 	t.Log(dataFile1)
 	t.Log(dir)
 }
+
+func TestDataFile_Write(t *testing.T) {
+	dataFile, err := OpenDataFile(dir, 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("bbb"))
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("ccc"))
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Close(t *testing.T) {
+	dataFile, err := OpenDataFile(dir, 123)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Close()
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Sync(t *testing.T) {
+	dataFile, err := OpenDataFile(dir, 456)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa"))
+	assert.Nil(t, err)
+
+	err = dataFile.Sync()
+	assert.Nil(t, err)
+}
